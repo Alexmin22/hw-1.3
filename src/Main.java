a0f2c6dc34b1c169f50212544cdf585e251a62a8
Alexmin22 <115101954+Alexmin22@users.noreply.github.com>
2023-08-07 00:20:04 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -168,4 +168,33 @@ public class Main {
 
         return res == Integer.MAX_VALUE ? 0 : res;
     }
+
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        //You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots. Given 
+        //an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return true if n new flowers can
+            //be planted in the flowerbed without violating the no-adjacent-flowers rule and false otherwise.
+        for(int i =0; i<flowerbed.length; i++) {
+                if(n==0) {
+                    return true;
+                }
+                if(i == 0 && i+1 <flowerbed.length  && flowerbed[i] == 0 && flowerbed[i+1] == 0) {
+                    flowerbed[i] =1;
+                    n--;
+                } else if(i == flowerbed.length-1 && i-1 > 0 && flowerbed[i] == 0 && flowerbed[i-1] == 0) {
+                    flowerbed[i] =1;
+                    n--;
+                }
+                if(flowerbed.length==1&&flowerbed[0]==0) {
+          return true;
+          }
+                if(flowerbed[i] == 0 && i+2 <flowerbed.length && flowerbed[i+1] == 0 && flowerbed[i+2] == 0) {
+                    flowerbed[i+1] =1;
+                    n--;
+                }
+                if(n==0) {
+                    return true;
+                }
+            }
+            return false;
+}
 }
