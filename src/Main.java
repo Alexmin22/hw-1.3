ee896c98345d4f52b7b29c808c3cfb35cc218999
Alexmin22 <115101954+Alexmin22@users.noreply.github.com>
2023-08-09 12:57:51 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -282,7 +282,25 @@ public int[] plusOne(int[] digits) {
 }
 
 
+public boolean isSubsequence(String s, String t) {
+    //Given two strings s and t, return true if s is a subsequence of t, or false otherwise. A subsequence of a string is a new string 
+    //that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions 
+    //of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+        int count = 0;
 
+        for(int i = 0; i<t.length(); i++) {
+            if(count == s.length()) {
+                return true;
+            }
+            if(s.charAt(count) == t.charAt(i)) {
+                count++;
+            }
+        }
+        if(count == s.length()) {
+                return true;
+            }
+        return false;
+    }
 
 
 
