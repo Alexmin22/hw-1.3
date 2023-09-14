3235918e6977b6efcd024d753fa2a7d30d28952d
Alexmin22 <115101954+Alexmin22@users.noreply.github.com>
2023-09-14 22:48:43 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -795,6 +795,18 @@ public boolean isIsomorphic(String s, String t) {
         }
         return nums[nums.length-1];
     }
+    
+//https://leetcode.com/problems/house-robber/
+    public int rob(int[] nums) {
+        for(int i = 1; i< nums.length; i++) {
+            if(i ==1) {
+                nums[i] =  Math.max(nums[0], nums[1]);
+            } else {
+                nums[i] = Math.max(nums[i-1], nums[i]+nums[i-2]);
+            }
+        }
+        return nums[nums.length-1];
+    }
 }
 
 
